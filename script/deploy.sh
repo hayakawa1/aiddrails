df18e836b0ab4abfa836374bff8b58213404c3b6
Hiroshi Hayakawa <hayakawa@bit-co.jp>
2025-03-08 00:01:00 +0900

    fix: デプロイスクリプトを修正

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,10 +4,14 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
+# データベース設定
+export RAILS_ENV=production
+export DATABASE_URL="postgres://ubuntu:ubuntu@localhost/ubuntu_production"
+
 cd ~/rails/myapp && \
 git pull origin main && \
 bundle config set --local without 'development test' && \
 bundle install && \
-RAILS_ENV=production bundle exec rails assets:precompile && \
-RAILS_ENV=production bundle exec rails db:migrate && \
-sudo systemctl restart puma 
+bundle exec rails assets:precompile && \
+bundle exec rails db:migrate && \
+systemctl restart puma 
