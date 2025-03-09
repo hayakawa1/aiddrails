dd9bdf14d89e4cef852343e7fe844b6b87e5a435
Hiroshi Hayakawa <hayakawa@bit-co.jp>
2025-03-09 18:51:24 +0900

    許可されたホスト名にHerokuアプリのドメインを追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,10 +81,11 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+  config.hosts = [
+    "aidd-5f4ef2dba244.herokuapp.com",     # Allow requests from Heroku app
+    # "example.com",     # Allow requests from example.com
+    # /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
+  ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
